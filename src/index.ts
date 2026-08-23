56075eb2f0b1daf3322593c7c2316eb04847a670
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-23 19:19:08 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as ClaudiaNotificationBell } from './ClaudiaNotificationBell';
+export type { ClaudiaNotificationBellProps } from './ClaudiaNotificationBell';
+export { default as ClaudiaNotificationList } from './ClaudiaNotificationList';
+export type { ClaudiaNotificationListProps, ClaudiaNotificationListCopy } from './ClaudiaNotificationList';
+export type { ClaudiaNotification } from './types';
